1ba243b3539d5af7c901aa023d30931eabc1f265
macbook <macbook@mariachmacbook2.station>
2019-04-23 10:03:47 +0200

    Progetto iniziale, classe voto vuota

---
+++ src/it/polito/tdp/libretto/Voto.java
@@ -0,0 +1,11 @@
+package it.polito.tdp.libretto;
+
+import java.time.LocalDate;
+
+public class Voto {
+	private int voto;
+	private String corso;
+	//mai usare una stringa per un oggetto ma usare localdate dal package time
+	private LocalDate data;
+
+}
